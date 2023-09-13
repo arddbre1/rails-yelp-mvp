fcf45706b1bf9be8303774ea12222769533b0041
ardian idrizi <ardianidizi@gmail.com>
2023-09-13 13:55:40 +0200

    Validated the Restaurant and Review models

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,2 +1,5 @@
 class Restaurant < ApplicationRecord
+  validates :name, :category, :address, presence: true
+  has_many :reviews, dependent: :destroy
+  validates :category, inclusion: { in: ["chinese", "italian", "japanese", "french", "belgian"] }
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,2 +1,6 @@
 class Review < ApplicationRecord
+  validates :content, :rating, presence: true
+  belongs_to :restaurant
+  validates :rating, numericality: { only_integer: true }
+  validates_inclusion_of :rating, :in => 0..5
 end
